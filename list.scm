f7a4271a18c910618f8ad797858083045eb24e92
nozomu.hirata <nozomu.hirata@jointcrew.co.jp>
2023-10-18 12:34:02 +0900

    feat: list

---
+++ list.scm
@@ -0,0 +1,15 @@
+(car '(1 2 3))
+(cdr '(1 2 3))
+(null? '(1 2))
+
+(define (sum-of-numbers lis)
+  (fold + 0 lis))
+
+(last-pair '(1 2 3 5))
+
+(define (last-p lis)
+  (if (pair? (cdr lis))
+    (last-pair (cdr lis))
+    lis))
+
+(for-each (lambda (x) (print "> " x)) '(a b c))
